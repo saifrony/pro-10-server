60993707564a9abcf676db1098689700f54d3d5e
saifRony <isaif326@gmail.com>
2022-12-29 01:49:10 +0600

    image add

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-const express = require('express');
+const express = require('express')
 const app = express();
-const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const cors =require('cors');
+
 app.use(cors());
 
 
-const Port = process.env.Port || 5000;
-const course = require("./data/course.json");
+const course =require('./data/course.json');
 
-app.get("/",(req,res)=>{
-    res.send("server is done")
-});
-app.get("/course",(req,res)=>{
+
+app.get('/course', (req, res)=> {
     res.send(course);
 });
-app.get("/course/:id",(req,res)=>{
-    const id = req.params.id;
-    const courss = course.find((p)=>p.id===id);
-    res.send(courss);
-})
 
-app.listen(Port,()=>{
-    console.log("server is run",Port);
-})
 
-module.exports = app;
+
+
+
+// app.get('/course/:id',(req,res)=>{  
+//     const id = req.params.id;
+//     const course= news.find(n=>n._id === id);
+//     res.send(course);
+    
+// });
+// app.get('/catagory/:id',(req,res)=>{
+    
+//     const id = req.params.id;
+//     if(id==='08'){
+//         res.send(news);
+//     }else{
+//         const cata_newses= news.filter(n => n.category_id === id);
+//         res.send (cata_newses);
+//     }
+   
+    
+// });
+
+
+app.listen(port,()=>{
+     console.log('dream news, port', port);
+})
